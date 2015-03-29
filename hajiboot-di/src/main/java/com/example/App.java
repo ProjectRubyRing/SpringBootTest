b849a31f5d8f027337ccab258b71aa306b2828bc
taka <taka0206@me.com>
2015-03-29 12:21:01 +0900

    DIサンプル

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -1,24 +1,24 @@
 package com.example;
 
-import com.example.app.Calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 
-import java.util.Scanner;
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
+import com.example.app.Calculator;
 
 @EnableAutoConfiguration
 @Import(AppConfig.class)
 public class App {
 	public static void main(String[] args) {
 		try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-			Scanner scanner = new Scanner(System.in);
 			System.out.println("Enter 2 numbers like 'a b' : ");
-			int a = scanner.nextInt();
-			int b = scanner.nextInt();
+			ArgumentResolver argumentResolver = context.getBean(ArgumentResolver.class);
+			Argument argument = argumentResolver.resolve(System.in);
 			Calculator calculator = context.getBean(Calculator.class);
-			int result = calculator.calc(a, b);
+			int result = calculator.calc(argument.getA(), argument.getB());
 			System.out.println("result = " + result);
 		}
 	}
